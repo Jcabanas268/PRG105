27b5c2f4e0abf90e107939109ed58ef315d3a25e
Jcabanas268 <77952376+Jcabanas268@users.noreply.github.com>
2021-02-02 20:21:39 -0600

    Add Mad Lib

---
+++ printingMadLib.py
@@ -0,0 +1,56 @@
+# Startup Pitch
+print("Your startup name ")
+startup_name = input("Ending in -ly or -io: ")
+print("Hello welcomE to " + startup_name + "!\n")
+
+# Get inputs
+verb = input("enter a verb: ")
+plural_noun = input("enter plural noun: ")
+minor_celebrity = input("enter a minor celebrity: ")
+gentrified_neighborhood = input("enter a gentrified neighborhood: ")
+obscure_ethnicity = input("enter an obscure ethnicity: ")
+noun = input("enter a noun: ")
+verb_ate = input("enter a verb ending in -ate: ")
+trivial_stuff = input("enter a trivial stuff you like: ")
+social_media = input("enter a social media profile no one reads: ")
+crowd_gerund = input("enter a gerund starting with 'crowd': ")
+failing_industry = input("enter a failing industry: ")
+creative_verb = input("enter a verb that implies creativity: ")
+noun_verb_ate = input("enter a noun form of verb ending in -ate: ")
+boring_task = input("enter a boring daily task: ")
+unmanageable_thing = input("enter an unmanageable thing: ")
+adjective = input("enter an adjective: ")
+acronym = input("enter a slightly intimidating acronym: ")
+previous_ipo = input("enter a previous startup with record-breaking IPO: ")
+first_world_problem = input("enter a first-world problem: ")
+euphemistic_gerund = input("enter a euphemistic gerund: ")
+face_to_face = input("enter a verb involving face-to-face interaction with humans: ")
+data = input("enter data no one needs to keep track of except advertisers: ")
+invasive_service = input("enter an invasive, data-mining GPS-based service: ")
+serious_verb = input("enter a serious verb: ")
+fun_verb = input("enter a fun verb: ")
+verb_startup = input("enter a verb form of startup name: ")
+
+# Print prompt
+print("\n")
+print(startup_name + ". " + "Have you ever wanted to " + verb + " all sorts of " + plural_noun + " with your friends, family, colleagues, or even " + minor_celebrity + ", " + "but didn't know how?\n")
+
+print("Maybe you were just strolling the sidewalks of " + gentrified_neighborhood + " past your favorite " + obscure_ethnicity + " bakery ")
+print("when you wished you could pull out your i-" + noun + " and use it quickly to " + verb_ate + " all your latest " + trivial_stuff + " and post it to your " + social_media + "?\n")
+
+print(startup_name + " is the answer. The fact is, we live in a world where " + crowd_gerund + " will revolutionize " + failing_industry + ", ")
+print("and pretty soon you'll be able to " + creative_verb + " anything you want through the cloud.\n")
+
+print("All this " + noun_verb_ate + " is already changing the way we " + boring_task + ", but who has the tools to keep up with it?\n")
+
+print(startup_name + " is that tool.\n")
+
+print("With our beautiful, user-friendly interface, you'll find that managing all your " + unmanageable_thing + " is easier than ever and " + adjective + "!\n")
+
+print("Utilizing the power of " + acronym + " services with all connectivity of " + previous_ipo + ", we can help you free yourself from the tyranny of " + first_world_problem + ".\n")
+
+print("In this rapidly " + euphemistic_gerund + " world of ours, it's hard to find the time to " + face_to_face + " or keep track of all the " + data + ".\n")
+
+print(startup_name + " changes that. It puts the power of " + invasive_service + " at your fingertips. " + serious_verb + ".\n")
+
+print(fun_verb + ". " + verb_startup + ".\n")
