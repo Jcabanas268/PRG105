ca2268dbdfd032ecf43c6a142bb8a40a65cf09ab
Jcabanas268 <77952376+Jcabanas268@users.noreply.github.com>
2021-02-08 18:05:40 -0600

    Update ch_2_budget_logic.py
    corrected double spacing for defining and calling functions lines 17, 34, 42, 43.

--- chapter_practice/ch_2_budget_logic.py
+++ chapter_practice/ch_2_budget_logic.py
@@ -1,6 +1,6 @@
-'''
+"""
 A program that helps someone create a budget. It should ask the user for monthly income and the following expenses:
-'''
+"""
 
 # var data
 income = float(input('What is your total net monthly income?: $'))
@@ -11,6 +11,7 @@ transportation = float(input('How much do you spend on transportation each month
 phone = float(input('How much do you spend on your phone bill each month?: $'))
 utilities = float(input('How much do you spend on your utilities each month?: $'))
 clothing = float(input('How much do you spend on clothing each month?: $'))
+
 
 # processing
 def func_budget_percent():
@@ -29,12 +30,14 @@ def func_budget_percent():
     print('Utilities takes up', format(utilities_budget, ',.2f'), '% of your monthly budget.')
     print('Clothing takes up', format(clothing_budget, ',.2f'), '% of your monthly budget.')
 
+    
 def func_take_home_pay():
     # difference for take home pay
     take_home_pay = (income - (housing + food + transportation + phone + utilities + clothing))
     # print take home pay
     print('You have $', format(take_home_pay, ',.2f'), 'left from your income after paying these monthly expenses.')
 
+    
 # output budget percent and take home pay
 func_budget_percent()
 func_take_home_pay()
