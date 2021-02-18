35ce6936a6486e5f5487f471be136f144b5f66f6
Jcabanas268 <77952376+Jcabanas268@users.noreply.github.com>
2021-02-18 16:05:31 -0600

    Update sales_program.py
    move avg_sales to # processing line 10

--- chapter_practice/chapter4/ch_4_program_sales/sales_program.py
+++ chapter_practice/chapter4/ch_4_program_sales/sales_program.py
@@ -1,12 +1,13 @@
 # sales program
 # data var
 total_sales = 0
-avg_sales = total_sales / 7     # getting average sale per day
+
 
 # processing
 for day in ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'):       # run through list
     get_day_sales = float(input('Enter day sales for ' + day + ': $ '))         # input sale for day
     total_sales += get_day_sales        # add day sale to total
+avg_sales = total_sales / 7     # getting average sale per day
 
 # output
 print('Total sales for week: $' + format(total_sales, ',.2f'))      # print total
